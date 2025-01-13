07fe33a92abb6e719180803bbb0539c24e2c5d92
SamueleTerraroli <terraroli99@gmail.com>
2025-01-13 18:31:22 +0100

    feat: add cardsection

---
+++ src/components/Main.jsx
@@ -0,0 +1,58 @@
+const Main = () => {
+  return (
+    <main>
+        <div className="content py-5">
+            <div className="container">
+                <p>-- Content Goes Here --</p>
+            </div>
+        </div>
+        <div className="menu">
+            <div className="container">
+                <div className="nav">
+                    <ul>
+                        <li>
+                            <a href="">
+                                <img src="../ass" alt=""/>
+                                <p>Digital comics</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="" alt=""/>
+                                <p>dc merchandise</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="" alt=""/>
+                                <p>subscription</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="" alt=""/>
+                                <p>comic shop locator</p>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="">
+                                <img src="" alt=""/>
+                                <p>dc power visa</p>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div className="fun-art">
+            <div className="container">
+                <nav>
+
+                </nav>
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default Main
