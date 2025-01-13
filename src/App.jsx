643730b79af0362001dbf4359ccc21473925a971
SamueleTerraroli <terraroli99@gmail.com>
2025-01-13 18:30:56 +0100

    feat: add header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,12 @@
+import Header from "./components/Header"
+import Main from "./components/Main"
+
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <Header />
+    <Main />
+  </>
   )
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+const Header = () => {
+  return (
+    <header className="container d-flex justify-content-between align-items-center">
+     <img src="/dc-logo.png" alt="Logo" />
+     <nav>
+        <ul>
+            <li><a href="#">characters</a></li>
+            <li><a href="#" className="active">comics</a></li>
+            <li><a href="#">movies</a></li>
+            <li><a href="#">tv</a></li>
+            <li><a href="#">games</a></li>
+            <li><a href="#">collectibles</a></li>
+            <li><a href="#">videos</a></li>
+            <li><a href="#">fans</a></li>
+            <li><a href="#">news</a></li>
+            <li><a href="#">shop</a></li>
+        </ul>
+     </nav> 
+    </header>
+  )
+}
+
+export default Header
